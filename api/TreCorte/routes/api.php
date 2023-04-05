8538972df867e6294750f3f3248817f46ea1deb3
Megyeri Márk Máté <megyko1234@gmail.com>
2023-04-05 11:19:38 +0200

    Javítva

--- api/TreCorte/routes/api.php
+++ api/TreCorte/routes/api.php
@@ -13,7 +13,7 @@ use App\Http\Controllers\UserController;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 
- /////AASD
+ 
 });
 //Authentikáció után sikeres műveletek
 
